fa00ff76617d220b94d81ee5d253ba4255f59651
Thomas Jewson <tjewson1@gmail.com>
2023-01-05 19:20:57 +0000

    feat(variance-within): Calculated the variance within

--- anova_from_scratch.py
+++ anova_from_scratch.py
@@ -9,4 +9,16 @@ df = pd.read_csv(DATA_LOC)
 df.head(5)
 # %%
 grand_mean = df["party"].mean()
+df["squared_error"] = (df["party"] - grand_mean) ** 2
+sst = df["squared_error"].sum()
+
 # %%
+usage_groups = df.groupby("marijUse")
+grouped_means = usage_groups["party"].mean()
+
+
+def get_variance_within(useage_group: pd.Series):
+    return (useage_group - grouped_means[useage_group.name]) ** 2
+
+
+df["variance_within"] = usage_groups["party"].apply(get_variance_within)
